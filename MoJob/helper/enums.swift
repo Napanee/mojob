ea381b6c1e4901de350fdd16ceba719c57278f6d
Martin Schneider <martin.schneider@moccu.com>
2019-02-09 19:34:40 +0100

    add nibname enum helper

---
+++ MoJob/helper/enums.swift
@@ -0,0 +1,14 @@
+//
+//  enums.swift
+//  MoJob
+//
+//  Created by Martin Schneider on 08.02.19.
+//  Copyright © 2019 Martin Schneider. All rights reserved.
+//
+
+enum nibNames {
+	static let NavigationController = "NavigationController"
+	static let JobListController = "JobListController"
+	static let FavoritesCollectionItem = "FavoriteItem"
+	static let JobsCollectionItem = "JobItem"
+}
