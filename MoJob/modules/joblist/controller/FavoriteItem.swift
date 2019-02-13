be3aee1511858f786e3167f7363af4b51b8e1c49
Martin Schneider <martin.schneider@moccu.com>
2019-02-13 22:24:52 +0100

    remove unused code

--- MoJob/modules/joblist/controller/FavoriteItem.swift
+++ MoJob/modules/joblist/controller/FavoriteItem.swift
@@ -18,8 +18,6 @@ class FavoriteItem: NSCollectionViewItem {
     }
 
 	@IBAction func startTracking(_ sender: NSButton) {
-		let vc = TrackingViewController(nibName: nibNames.TrackingViewController, bundle: nil)
-		let animator = CustomAnimator()
 		let window = (NSApp.delegate as! AppDelegate).window
 
 		if let contentViewController = window?.contentViewController as? SplitViewController {
