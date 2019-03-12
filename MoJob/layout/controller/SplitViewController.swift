a9cf7fe7b0dfcc32f4252d0914caefd3dc4ce06f
Martin Schneider <martin.schneider@moccu.com>
2019-03-12 20:49:40 +0100

    update splitviewcontroller, to keep the width of previous view

--- MoJob/layout/controller/SplitViewController.swift
+++ MoJob/layout/controller/SplitViewController.swift
@@ -10,12 +10,29 @@ import Cocoa
 
 class SplitViewController: NSSplitViewController {
 
+	var prevWidth: CGFloat?
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		if (splitViewItems.count == 3) {
 			splitView.autosaveName = "save_divider"
 		}
+	}
+
+	override func removeSplitViewItem(_ splitViewItem: NSSplitViewItem) {
+		super.removeSplitViewItem(splitViewItem)
+
+		prevWidth = splitViewItem.viewController.view.bounds.width
+	}
+
+	override func insertChild(_ childViewController: NSViewController, at index: Int) {
+		if let prevWidth = prevWidth {
+			childViewController.view.frame.size.width = prevWidth
+			self.prevWidth = nil
+		}
+
+		super.insertChild(childViewController, at: index)
 	}
 
 	override func splitView(_ splitView: NSSplitView, effectiveRect proposedEffectiveRect: NSRect, forDrawnRect drawnRect: NSRect, ofDividerAt dividerIndex: Int) -> NSRect {
