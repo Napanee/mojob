0d2c70a7397e8432a5e26b8a236ec289f65b0d93
Martin Schneider <martin.schneider@moccu.com>
2019-03-27 13:51:49 +0100

    add bridging header

---
+++ MoJob/MoJob-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  MoJob-Bridging-Header.h
+//  MoJob
+//
+//  Created by Martin Schneider on 25.03.19.
+//  Copyright © 2019 Martin Schneider. All rights reserved.
+//
+
+#import <CommonCrypto/CommonCrypto.h>
