2a9b4a48aceb28299964e7a0d1bf660427097cfb
Martin Schneider <martin.schneider@moccu.com>
2019-03-29 21:54:23 +0100

    remove tracking area

--- MoJob/modules/joblist/views/CollectionItemView.swift
+++ MoJob/modules/joblist/views/CollectionItemView.swift
@@ -11,7 +11,6 @@ import Cocoa
 class CollectionItemView: NSView, CAAnimationDelegate {
 
 	let backgroundLayer = CALayer()
-	var trackingArea: NSTrackingArea?
 
 	@IBInspectable weak var backgroundColor: NSColor! = NSColor.clear
 
@@ -24,53 +23,14 @@ class CollectionItemView: NSView, CAAnimationDelegate {
 		layer?.insertSublayer(backgroundLayer, at: 0)
 	}
 
-	override func mouseEntered(with event: NSEvent) {
-		backgroundLayer.backgroundColor = backgroundColor.cgColor
-
-		let animation = CABasicAnimation(keyPath: "backgroundColor")
-		animation.fromValue = CGColor.clear
-		animation.toValue = backgroundColor.cgColor
-		animation.duration = 0.5
-		animation.delegate = self
-		backgroundLayer.add(animation, forKey: "bgColor")
-	}
-
 	override func mouseExited(with event: NSEvent) {
 		backgroundLayer.removeAllAnimations()
 		backgroundLayer.backgroundColor = CGColor.clear
 	}
 
-	override func updateTrackingAreas() {
-		super.updateTrackingAreas()
-
-		if let trackingArea = trackingArea {
-			removeTrackingArea(trackingArea)
-		}
-
-		trackingArea = NSTrackingArea(
-			rect: bounds,
-			options: [
-				NSTrackingArea.Options.activeAlways,
-				NSTrackingArea.Options.mouseEnteredAndExited
-			],
-			owner: self,
-			userInfo: nil
-		)
-
-		addTrackingArea(trackingArea!)
-	}
-
 	func highlightBackground(is visible: Bool) {
-		print(visible)
 		if (visible) {
 			backgroundLayer.backgroundColor = backgroundColor.cgColor
-
-//			let animation = CABasicAnimation(keyPath: "backgroundColor")
-//			animation.fromValue = CGColor.clear
-//			animation.toValue = backgroundColor.cgColor
-//			animation.duration = 0.5
-//			animation.delegate = self
-//			backgroundLayer.add(animation, forKey: "bgColor")
 		} else {
 			backgroundLayer.backgroundColor = CGColor.clear
 		}
